e3e9f8624c183be07f4a0521f50e6f163be6c315
Berk620 <kifmyvz@gmail.com>
2023-01-28 12:31:27 +0100

    Pushing TCs to solution branch

--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
@@ -16,23 +16,33 @@ public class WhenWorkingWithFloatingPointNumbers {
         double celcius = 27.0d;
         double farenheit = 0.0d;
 
+        farenheit = degreeConverter(celcius);
+
         // TODO: Use a floating point calculation to calculate the farenheit equivalent of the celcius value.
 
         assertThat(farenheit, equalTo(80.6));
     }
 
+    public double degreeConverter(double celcius){
+        return (double) 9/5 * celcius + 32;
+    }
+
     //
     // Exercise 2: Convert kilograms to pounds
-    // There are 2.20462 pounds in a kilogram
+    // There are 2.20462 pounds in a kilogram 1kg=2.20462 pounds
     //
     @Test
     public void convertMetersToFeet() {
         int weightInKilograms = 50;
         double weightInPounds = 0;
+        weightInPounds = meterConverter(weightInKilograms);
 
         // TODO: Use a floating point calculation to calculate the correct weight in pounds
-
         assertThat(weightInPounds, equalTo(110.231));
 
+    }
+
+    public  double meterConverter(double kilograms){
+        return (float) kilograms *2.20462;
     }
 }
